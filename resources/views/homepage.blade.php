7a3d3977bb583b73b9052ce910cd7fbc88ede14e
Luigi1201 <luigi.riggi1201@gmail.com>
2021-04-28 18:46:38 +0200

    2

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,9 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{asset('css/homepage.css')}}" type="text/css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
     <title>HOMEPAGE NOMADLIST</title>
 </head>
 <body>
-    homepage
+    <div class="continer-fluid" id="videoContainer">
+        <video autoplay muted loop id="video">
+            <source src="media/HeaderVideo.mp4" type="video/mp4">
+        </video>        
+    </div>
+    <div class="Button"> Pulsante </div>
+    <div class="container" id="principalContent">
+        <div class="row">
+
+        </div>        
+    </div>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('homepage');
 });
 
 Route::get('/nomadList', function () {
