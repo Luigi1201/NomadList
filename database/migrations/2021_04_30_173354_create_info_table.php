6728c0220a6e2fbb45b147540be39594a6751dfa
Luigi1201 <luigi.riggi1201@gmail.com>
2021-04-30 19:38:24 +0200

    first migration

---
+++ database/migrations/2021_04_30_173354_create_info_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInfoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('info', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->charset = 'utf8mb4';
+            $table->bigIncrements('id');
+            $table->timestamps();
+            //inserire ancora temperatura,nrAbitanti,velocitàConnessione
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('info');
+    }
+}
