8137843d77f76988de0703a0d9e61ac68da11e3f
Luigi1201 <luigi.riggi1201@gmail.com>
2021-05-06 17:57:35 +0200

    aggiornamentoHomepageStyle

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -13,18 +13,25 @@
         <div class="row">
             <video autoplay muted loop id="video">
                 <source src="media/HeaderVideo.mp4" type="video/mp4">
-            </video> 
-        </div> 
+            </video>  
+        </div>
         <svg viewBox="0 0 1440 120" class="wave">
             <path d="M1440,21.2101911 L1440,120 L0,120 L0,21.2101911 C120,35.0700637 240,42 360,42 C480,42 600,35.0700637 720,21.2101911 C808.32779,12.416393 874.573633,6.87702029 918.737528,4.59207306 C972.491685,1.8109458 1026.24584,0.420382166 1080,0.420382166 C1200,0.420382166 1320,7.35031847 1440,21.2101911 Z">
             </path>
-        </svg>    
-        <div class="Button"> Pulsante </div>  
+        </svg> 
+        <div class="row" id="onVideoContents">
+            <div class="col-12 col-md-7">
+                <div class="GoNomad" onclick="location.href='/register'">
+                    <h1 style="color: white">
+                        Go nomad
+                    </h1>
+                </div>
+            </div>
+            <div class="col-0 col-md-5">
+                <div class="ButtonLogin"> Pulsante </div>     
+            </div> 
+        </div>  
     </div>
-    <div class="container" id="principalContent">
-        <div class="row">
-            
-        </div>        
-    </div>
+    
 </body>
 </html>
