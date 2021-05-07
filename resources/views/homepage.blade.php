12b6a071fccd6d5c23789bf0e274704d2c054a87
Luigi1201 <luigi.riggi1201@gmail.com>
2021-05-07 21:37:06 +0200

    upload layout

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -2,35 +2,43 @@
 <html lang="it">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0", shrink-to-fit=no>
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="{{asset('css/homepage.css')}}" type="text/css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
-    <title>HOMEPAGE NOMADLIST</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <title>NomadList — I migliori posti in cui vivere per un nomade digitale</title>
 </head>
-<body>
-    <div class="continer-fluid" id="videoContainer">
-        <div class="row">
-            <video autoplay muted loop id="video">
-                <source src="media/HeaderVideo.mp4" type="video/mp4">
-            </video>  
-        </div>
-        <svg viewBox="0 0 1440 120" class="wave">
-            <path d="M1440,21.2101911 L1440,120 L0,120 L0,21.2101911 C120,35.0700637 240,42 360,42 C480,42 600,35.0700637 720,21.2101911 C808.32779,12.416393 874.573633,6.87702029 918.737528,4.59207306 C972.491685,1.8109458 1026.24584,0.420382166 1080,0.420382166 C1200,0.420382166 1320,7.35031847 1440,21.2101911 Z">
-            </path>
-        </svg> 
+<body leftmargin="0" topmargin="0">
+    <div class="videoContainer">
+        <video autoplay muted loop id="video">
+            <source src="media/HeaderVideo.mp4" type="video/mp4">
+        </video> 
+    </div>
+    <div class="container-fluid">
         <div class="row" id="onVideoContents">
-            <div class="col-12 col-md-7">
-                <div class="GoNomad" onclick="location.href='/register'">
-                    <h1 style="color: white">
-                        Go nomad
-                    </h1>
+            <div class="col-7"
+                <div class="col-12 col-md-7">
+                    <div class="GoNomad" onclick="location.href='/register'">
+                        <h1 style="color: white">
+                            Go nomad
+                        </h1>
+                    </div>
                 </div>
+            </div>  
+            <div class="col-5">
+                <div class="ButtonLogin">
+                    <form action="login">
+                        <button class="btn btn-block btn-danger btn-lg">Login</button>  
+                    </form>
+                </div> 
             </div>
-            <div class="col-0 col-md-5">
-                <div class="ButtonLogin"> Pulsante </div>     
-            </div> 
-        </div>  
+        </div>
+        <div class="wave">
+            <svg viewBox="0 0 1440 120">
+                <path d="M1440,21.2101911 L1440,120 L0,120 L0,21.2101911 C120,35.0700637 240,42 360,42 C480,42 600,35.0700637 720,21.2101911 C808.32779,12.416393 874.573633,6.87702029 918.737528,4.59207306 C972.491685,1.8109458 1026.24584,0.420382166 1080,0.420382166 C1200,0.420382166 1320,7.35031847 1440,21.2101911 Z">
+                </path>
+             </svg> 
+        </div>
     </div>
     
 </body>
