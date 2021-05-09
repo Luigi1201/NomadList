84b699275e4c4f6fcffb4b7c692f2a122dd35c9f
Luigi1201 <luigi.riggi1201@gmail.com>
2021-05-09 10:34:13 +0200

    Simple models created

---
+++ app/Models/Info_generali.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Info_generali extends Model
+{
+    use HasFactory;
+    protected $table = 'info_generali';
+}
---
+++ app/Models/Info_meteo.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Info_meteo extends Model
+{
+    use HasFactory;
+    protected $table = 'info_meteo';
+}
